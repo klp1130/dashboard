1f06fc2b5cf959c29474c3bbf64c6e394502287f
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-08-12 17:23:19 -0600

    imports to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { SiShopware } from 'react-icons'
+import { MdOutlineCancel } from 'react-icons/md'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+
+import { links } from '../data/dummy'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+
+    </div>
+  
   )
 }
 
