e3ebc4105906556ea2711d32e716965a2852c78c
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-08-19 07:24:31 -0600

    added charts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings, LineChart } from './components'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers
-, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
+, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages'
 
 import { useStateContext } from './contexts/ContextProvider';
 
@@ -72,7 +72,7 @@ const App = () => {
             <Route path='/color-picker' element={<ColorPicker />} />
 
             {/* Charts */}
-            <Route path='/line' element={<LineChart />} />
+            <Route path='/line' element={<Line />} />
             <Route path='/area' element={<Area />} />
             <Route path='/bar' element={<Bar />} />
             <Route path='/pie' element={<Pie />} />
