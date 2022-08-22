91d9100fa37da62a960c6421fe2b5f7e0bcc9722
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-08-22 15:23:13 -0600

    gmail page complete

--- src/pages/Gmail.jsx
+++ src/pages/Gmail.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-
+import { Button } from '../components'
+import { FcGoogle } from 'react-icons/fc'
 import { Header } from '../components'
+import { useStateContext } from '../contexts/ContextProvider'
 
 const Gmail = () => {
+
+  const { currentColor } = useStateContext()
+
   return (
     <div className='m-2, md:m-10 mt-24 p-2
     md:p-10 bg-white rounded-3xl'>
       <Header category='App' title='Gmail' />  
-     
+      <div className='flex justify-center '> 
+        <Button
+                color='white'
+                bgColor={currentColor}
+                text='Login'
+                borderRadius='10px'
+                size='md'
+              />
+              <span>
+              <FcGoogle />
+              </span>
+     </div>
     </div>
   )
 }
